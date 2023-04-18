7c35e85ccedcfb6c3b2185b3657c149a6029311d
Yenleng <yenlengggggg@gmail.com>
2023-04-18 14:35:05 +0800

    initial commit

---
+++ src/main/java/com/yl/bankacc/App.java
@@ -0,0 +1,13 @@
+package com.yl.bankacc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/yl/bankacc/AppTest.java
@@ -0,0 +1,20 @@
+package com.yl.bankacc;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
