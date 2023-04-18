cc06be3d95435de29b85b5997b192462fde60be1
Yenleng <yenlengggggg@gmail.com>
2023-04-18 23:14:44 +0800

    add FD Constructor & getBalance

--- src/main/java/com/yl/bankacc/App.java
+++ src/main/java/com/yl/bankacc/App.java
@@ -1,13 +1,3 @@
 package com.yl.bankacc;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-}
+
